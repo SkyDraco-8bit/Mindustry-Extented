7da637fddfb67c199d27e279447080dc191f12ac
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-21 20:55:57 +0100

    Update and rename additive-reconstructor.js to multiplicative-reconstructor.js

--- content/blocks/factory/multiplicative-reconstructor.js
+++ content/blocks/factory/multiplicative-reconstructor.js
@@ -1,7 +1,8 @@
 @override
-Blocks.additiveReconstructor.upgrades.add(
+Blocks.multiplicativeReconstructor.upgrades.add(
     Seq.with(
        Vars.content.getByName(ContentType.unit, "mindustry-poly"),
+        Vars.content.getByName(ContentType.unit, "mindustry-mega")
         Vars.content.getByName(ContentType.unit, "exdustry-spirit")
     ).toArray(UnitType)
 );

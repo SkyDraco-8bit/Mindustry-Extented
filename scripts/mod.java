e86c83cf258261e2f09eb748ac1994aa603753d1
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-21 17:37:33 +0100

    Update and rename mod.java to mod.js

--- scripts/mod.java
+++
@@ -1,5 +0,0 @@
-var mod = Vars.mods.locateMod("Mindustry Ex");
-var x = mod.meta;
-x.displayName = "[#af6ecc]Braindustry";
-x.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
-x.author = "[#af6ecc]PixaxeOfPixie";
---
+++ scripts/mod.js
@@ -0,0 +1,5 @@
+var mod = Vars.mods.locateMod("Exdustry");
+var x = mod.meta;
+x.displayName = "[#af6ecc]Mindustry extented";
+x.description = "[#f2ed5e]Mindustry mod in developpement";
+x.author = "[#af6ecc]SkyDraci";

d37ae7f15f0179077ff4c08f10b9a8445e38642f
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-21 15:32:40 +0100

    Create mod.java

---
+++ scripts/mod.java
@@ -0,0 +1,5 @@
+var mod = Vars.mods.locateMod("Mindustry Ex");
+var x = mod.meta;
+x.displayName = "[#af6ecc]Braindustry";
+x.description = "[#f2ed5e]Huge mindustry mod, orienter to  end-game content.";
+x.author = "[#af6ecc]PixaxeOfPixie";

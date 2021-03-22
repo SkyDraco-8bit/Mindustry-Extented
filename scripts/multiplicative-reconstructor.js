b93ab1478ca9cc53dcf837104198db37bee6a866
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-22 15:54:57 +0100

    Update multiplicative-reconstructor.js

--- scripts/multiplicative-reconstructor.js
+++ scripts/multiplicative-reconstructor.js
@@ -2,15 +2,33 @@ package mindustry.content;
 
 import arc.graphics.*;
 import arc.struct.*;
-import mindustry.ai.types.*;
-import mindustry.annotations.Annotations.*;
+import mindustry.*;
 import mindustry.ctype.*;
-import mindustry.entities.abilities.*;
 import mindustry.entities.bullet.*;
 import mindustry.gen.*;
 import mindustry.graphics.*;
 import mindustry.type.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.experimental.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
+import mindustry.world.draw.*;
 import mindustry.world.meta.*;
+
+import static mindustry.type.ItemStack.*;
 
 @Override
 Blocks.multiplicativeReconstructor.upgrades.add(

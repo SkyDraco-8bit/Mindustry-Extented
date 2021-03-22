a01a454e86bdbed963513d86ff98a3037174b672
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-22 15:01:17 +0100

    Update and rename wormcomp.java to UnitTypes.js

---
+++ scripts/UnitTypes.js
@@ -0,0 +1,19 @@
+package mindustry.content;
+
+import arc.graphics.*;
+import arc.struct.*;
+import mindustry.ai.types.*;
+import mindustry.annotations.Annotations.*;
+import mindustry.ctype.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+//ground region
+public class UnitTypes implements ContentList{
+    public static @EntityDef({Unitc.class, Wormc.class}) UnitType testworm;
+}

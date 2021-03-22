758e589af92a4bfb85c0078df45c7b676597bda6
SkyDraco-8bit <80839516+SkyDraco-8bit@users.noreply.github.com>
2021-03-22 15:44:28 +0100

    Update multiplicative-reconstructor.js

--- scripts/multiplicative-reconstructor.js
+++ scripts/multiplicative-reconstructor.js
@@ -17,6 +17,7 @@ Blocks.multiplicativeReconstructor.upgrades.add(
     Seq.with(
        Vars.content.getByName(ContentType.unit, "mindustry-poly"),
         Vars.content.getByName(ContentType.unit, "mindustry-mega")
+       Vars.content.getByName(ContentType.unit, "mindustry-poly"),
         Vars.content.getByName(ContentType.unit, "exdustry-spirit")
     ).toArray(UnitType)
 );
